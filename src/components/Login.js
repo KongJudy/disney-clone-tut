bc56c1ea814cfd94d0cfc5bfd0ce589541d0314e
Judy Kong <judy.kong817@gmail.com>
2023-07-11 15:33:30 -0400

    login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,15 @@ const Login = (props) => {
   return (
     <Container>
       <Content>
+        <CTA>
+          <CTALogoOne src='/images/cta-logo-one.svg' alt='' />
+          <SignUp>GET IT ALL THERE</SignUp>
+          <Description>
+            Get Premier Access to Raya and the Last Dragon with a Disney+
+            subscription.
+          </Description>
+          <CTALogoTwo src='/images/cta-logo-two.png' alt='' />
+        </CTA>
         <BgImage />
       </Content>
     </Container>
@@ -43,6 +52,64 @@ const BgImage = styled.div`
   right: 0;
   left: 0;
   z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity: .2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0663e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px; 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
+`;
+
+const Description = styled.h2`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 12px;
+  margin: 0 0 24px;
+  line-height: 1.5;
+  letter-spacing: 1.5px;
+`;
+
+const CTALogoTwo = styled.img`
+  max-width: 600px;
+  margin-bottom: 20px;
+  display: inline-block;
+  vertical-align: bottom;
+  width: 100%;
 `;
 
 export default Login;

2005ef61db9e997f2f6ec0e19d5e93b48ec04ec7
Judy Kong <judy.kong817@gmail.com>
2023-07-12 20:51:17 -0400

    add subtitle and description

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -21,7 +21,22 @@ const Detail = (props) => {
             <img src='/images/play-icon-black.png' alt='' />
             <span>Play</span>
           </Player>
+          <Trailer>
+            <img src='/images/play-icon-white.png' alt='' />
+            <span>Trailer</span>
+          </Trailer>
+          <AddList>
+            <span />
+            <span />
+          </AddList>
+          <GroupWatch>
+            <div>
+              <img src='/images/group-icon.png' alt='' />
+            </div>
+          </GroupWatch>
         </Controls>
+        <SubTitle>SubTitle</SubTitle>
+        <Description>Description</Description>
       </ContentMeta>
     </Container>
   );
@@ -118,6 +133,85 @@ const Player = styled.button`
     img {
       width: 25px;
     }
+  }
+`;
+
+const Trailer = styled(Player)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddList = styled.div`
+  margin-right: 16px;
+  height: 44px;
+  width: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+
+  span {
+    background-color: rgb(249, 249, 249);
+    display: inline-block;
+
+    &:first-child {
+      height: 2px;
+      transform: translate(1px, 0px) rotate(0deg);
+      width: 16px;
+    }
+
+    &:nth-child(2) {
+      height: 16px;
+      transform: translateX(-8px) rotate(0deg);
+      width: 2px;
+    }
+  }
+`;
+
+const GroupWatch = styled.div`
+  height: 44px;
+  width: 44px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: white;
+
+  div {
+    height: 40px;
+    width: 40px;
+    background: rgb(0, 0, 0);
+    border-radius: 50%;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+  }
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  padding: 16px 0px;
+  color: rbg(249, 249, 249);
+
+  @media (max-width: 768) {
+    font-size: 14px;
   }
 `;
 
